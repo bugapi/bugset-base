86f3a73caae5b6ac7707ff321c361ae9671c52f5
butingdejiaobu <xingwangcom@163.com>
2019-10-18 10:51:41 +0800

    创建了一个dev分支

--- bugset-base-constant/src/main/java/org/bugapi/bugset/base/constant/UrlType.java
+++ bugset-base-constant/src/main/java/org/bugapi/bugset/base/constant/UrlType.java
@@ -11,42 +11,52 @@ public class UrlType {
 	 * 针对ClassPath路径的伪协议前缀（兼容Spring）: "classpath:"
 	 */
 	public static final String CLASSPATH_URL_PREFIX = "classpath:";
+
 	/**
 	 * URL 前缀表示文件: "file:"
 	 */
 	public static final String FILE_URL_PREFIX = "file:";
+
 	/**
 	 * URL 前缀表示jar: "jar:"
 	 */
 	public static final String JAR_URL_PREFIX = "jar:";
+
 	/**
 	 * URL 前缀表示war: "war:"
 	 */
 	public static final String WAR_URL_PREFIX = "war:";
+
 	/**
 	 * URL 协议表示文件: "file"
 	 */
 	public static final String URL_PROTOCOL_FILE = "file";
+
 	/**
 	 * URL 协议表示Jar文件: "jar"
 	 */
 	public static final String URL_PROTOCOL_JAR = "jar";
+
 	/**
 	 * URL 协议表示zip文件: "zip"
 	 */
 	public static final String URL_PROTOCOL_ZIP = "zip";
+
 	/**
 	 * URL 协议表示WebSphere文件: "wsjar"
 	 */
 	public static final String URL_PROTOCOL_WSJAR = "wsjar";
+
 	/**
 	 * URL 协议表示JBoss zip文件: "vfszip"
 	 */
 	public static final String URL_PROTOCOL_VFSZIP = "vfszip";
+
 	/**
 	 * URL 协议表示JBoss文件: "vfsfile"
 	 */
 	public static final String URL_PROTOCOL_VFSFILE = "vfsfile";
+
 	/**
 	 * URL 协议表示JBoss VFS资源: "vfs"
 	 */
